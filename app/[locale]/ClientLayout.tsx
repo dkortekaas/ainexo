c85ed2523e992a0127410335564e09172f85d3ff
Dennis Kortekaas <d.kortekaas@icloud.com>
2025-12-28 15:50:54 +0100

    Cleanup

--- app/[locale]/ClientLayout.tsx
+++ app/[locale]/ClientLayout.tsx
@@ -1,10 +1,11 @@
-import { HeroSection } from "@/components/site/Hero";
-import { HowItWorksSection } from "@/components/site/HowItWorks";
-import { CTASection } from "@/components/site/CTA";
-// import { TrustedBySection } from "@/components/site/TrustedBy";
-import { FeaturesSection } from "@/components/site/Features";
-import { PricingSection } from "@/components/site/Pricing";
-import { TestimonialsSection } from "@/components/site/Testimonials";
+import {
+  HeroSection,
+  HowItWorksSection,
+  CTASection,
+  FeaturesSection,
+  PricingSection,
+  TestimonialsSection,
+} from "@/components/site";
 import type { HeroSection as HeroSectionType } from "@/sanity/lib/homepage";
 import type { ChatWidget } from "@/sanity/lib/fetch";
 
---
+++ components/site/index.tsx
@@ -0,0 +1,7 @@
+export * from "./Hero";
+export * from "./HowItWorks";
+export * from "./CTA";
+export * from "./Features";
+export * from "./Pricing";
+export * from "./Testimonials";
+export * from "./TrustedBy";
